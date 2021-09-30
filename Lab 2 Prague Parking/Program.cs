04f2fae178f644906536094164e65cee601b26db
samuel andersson <samuel.ander@hotmail.com>
2021-09-30 11:31:14 +0200

    version 0.1

--- Lab 2 Prague Parking/Program.cs
+++ Lab 2 Prague Parking/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab_2_Prague_Parking
 {
@@ -6,11 +7,47 @@ namespace Lab_2_Prague_Parking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-        static void PragueParking(string[]Input)
-        {
+            for (int i = 0; i < 1;)
+            {
+                {
+                    Console.WriteLine("put in what Vechal it is and it's regristation number\n MC or CAR");
+                    string inout = Console.ReadLine();
+                    if (inout == "n")
+                        Environment.Exit(0);
+                    else
+                        PragueParking(inout);
 
+                }
+            }
+        }
+
+
+        static void PragueParking(string Input)
+        {
+            for (int length = 0; length < Input.Length; length++)
+                if (char.IsWhiteSpace(Input[length]))
+                {
+                    string test = Input.Replace(" ", "#");
+                    Console.WriteLine(test);
+                }
+                else if (!char.IsWhiteSpace(Input[length]))
+                {
+
+                }
+            //string[] parkingGarage = new string[100];
+            //Random random = new Random();
+            //int R = random.Next(0, parkingGarage.Length);
+
+            //for (int i = 0; parkingGarage.Length > i; i++)
+            //{
+            //    parkingGarage[R] = Input;
+
+            //    int ind = Array.IndexOf(parkingGarage, Input);
+
+            //    Console.WriteLine(parkingGarage[R]);
+            //    Console.WriteLine(ind + 1);
+            //    break;
+            //}
         }
     }
 }
