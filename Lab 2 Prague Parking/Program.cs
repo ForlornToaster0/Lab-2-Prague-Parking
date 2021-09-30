4886463a27b1d0816fcd03298acec21725534a1f
samuel andersson <samuel.ander@hotmail.com>
2021-09-30 09:23:17 +0200

    Add project files.

---
+++ Lab 2 Prague Parking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab_2_Prague_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void PragueParking(string[]Input)
+        {
+
+        }
+    }
+}
