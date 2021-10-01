242689c5f44a538e17a780cd250478878eb3066c
samuel andersson <samuel.ander@hotmail.com>
2021-10-01 10:51:26 +0200

    0.2.5

--- Lab 2 Prague Parking/Program.cs
+++ Lab 2 Prague Parking/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab_2_Prague_Parking
 {
@@ -10,44 +9,65 @@ namespace Lab_2_Prague_Parking
             for (int i = 0; i < 1;)
             {
                 {
-                    Console.WriteLine("put in what Vechal it is and it's regristation number\n MC or CAR");
-                    string inout = Console.ReadLine();
-                    if (inout == "n")
-                        Environment.Exit(0);
-                    else
-                        PragueParking(inout);
-
+                    Console.WriteLine("Put in a command\n(N)ew vehicle\n(F)ind vehicle\n(R)emove vehicle\n(A)ll spots\n(Q)uite");
+                    string MainInput = Console.ReadLine();
+                    ParkingSorting(MainInput);
+                    //if (MainInput == "N")
+                    //{
+                    //    Console.WriteLine("What kind of vehicle do you have and what is it's registration number?");
+                    //    string MainInput2 = Console.ReadLine();
+                    //    ParkingSorting(MainInput2);
+                    //}
+                    //else if (MainInput == "Q")
+                    //{
+                    //    Environment.Exit(0);
+                    //}
                 }
             }
+            Console.WriteLine("Are you parking a car or a MC?");
+            string input = (Console.ReadLine());
         }
 
-
-        static void PragueParking(string Input)
+        public static string[] parkingGarage = new string[100];
+        static void ParkingSorting(string Input)
         {
-            for (int length = 0; length < Input.Length; length++)
-                if (char.IsWhiteSpace(Input[length]))
+            string test="";
+            int i = 0;
+            while (i < parkingGarage.Length)
+            {
+                for (int length = 0; length < Input.Length; length++)
                 {
-                    string test = Input.Replace(" ", "#");
-                    Console.WriteLine(test);
+                    if (char.IsWhiteSpace(Input[length]))
+                    {
+                        test = Input.Replace(" ", "#");
+                        //Console.WriteLine(test);
+                    }
                 }
-                else if (!char.IsWhiteSpace(Input[length]))
+
+                Random random = new Random();
+                int R = random.Next(0, parkingGarage.Length);
+
+                if (parkingGarage[R] == null)
                 {
+                    i++;
+                    parkingGarage[R] = test;
 
+                    int ind = Array.IndexOf(parkingGarage, Input);
+
+                    Console.WriteLine("your spot is {0} {1}", ind, parkingGarage[R]);
                 }
-            //string[] parkingGarage = new string[100];
-            //Random random = new Random();
-            //int R = random.Next(0, parkingGarage.Length);
+                else
+                {
+                    Console.WriteLine("IT WORKS!!!!");
+                    i = 0;
+                }
+                break;
+            }
+        }
+        static void ParkingRemoving(string Input)
+        {
 
-            //for (int i = 0; parkingGarage.Length > i; i++)
-            //{
-            //    parkingGarage[R] = Input;
-
-            //    int ind = Array.IndexOf(parkingGarage, Input);
-
-            //    Console.WriteLine(parkingGarage[R]);
-            //    Console.WriteLine(ind + 1);
-            //    break;
-            //}
         }
     }
 }
+
