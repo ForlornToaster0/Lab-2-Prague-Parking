ab337156ea415f92991360581c48a2d4b7d52805
samuel andersson <samuel.ander@hotmail.com>
2021-10-07 09:11:57 +0200

    0.3 building up with MC

--- Lab 2 Prague Parking/Program.cs
+++ Lab 2 Prague Parking/Program.cs
@@ -10,58 +10,76 @@ namespace Lab_2_Prague_Parking
             {
                 {
                     Console.WriteLine("Put in a command\n(N)ew vehicle\n(F)ind vehicle\n(R)emove vehicle\n(A)ll spots\n(Q)uite");
-                    string MainInput = Console.ReadLine();
-                    ParkingSorting(MainInput);
-                    //if (MainInput == "N")
+                    string subInout1 = Console.ReadLine();
+                    string mainInput1 = subInout1.ToUpper();
+                    ParkingSorting(mainInput1);
+                    //if (mainInput1 == "N")
                     //{
                     //    Console.WriteLine("What kind of vehicle do you have and what is it's registration number?");
-                    //    string MainInput2 = Console.ReadLine();
-                    //    ParkingSorting(MainInput2);
+                    //    string subInput2 = Console.ReadLine();
+                    //    string mainInput2 = subInput2.ToUpper();
+                    //    ParkingSorting(mainInput2);
                     //}
-                    //else if (MainInput == "Q")
+                    //else if (mainInput1 == "Q")
                     //{
                     //    Environment.Exit(0);
                     //}
                 }
             }
-            Console.WriteLine("Are you parking a car or a MC?");
-            string input = (Console.ReadLine());
         }
-
         public static string[] parkingGarage = new string[100];
         static void ParkingSorting(string Input)
         {
-            string test="";
-            int i = 0;
-            while (i < parkingGarage.Length)
+            string test = "";
+            int activeProgram = 0;
+            Random random = new Random();
+            int R = random.Next(0, 2);
+            while (activeProgram < parkingGarage.Length)
             {
                 for (int length = 0; length < Input.Length; length++)
                 {
                     if (char.IsWhiteSpace(Input[length]))
                     {
                         test = Input.Replace(" ", "#");
-                        //Console.WriteLine(test);
                     }
                 }
-
-                Random random = new Random();
-                int R = random.Next(0, parkingGarage.Length);
-
                 if (parkingGarage[R] == null)
                 {
-                    i++;
+                    activeProgram++;
                     parkingGarage[R] = test;
+                    Console.WriteLine("your spot is {0} {1}", R + 1, parkingGarage[R]);
 
-                    int ind = Array.IndexOf(parkingGarage, Input);
 
-                    Console.WriteLine("your spot is {0} {1}", ind, parkingGarage[R]);
+
                 }
-                else
+                else if (parkingGarage[R].IndexOf("MC", 0, 2) > -1)
                 {
-                    Console.WriteLine("IT WORKS!!!!");
-                    i = 0;
+                    int pos = 0;
+                    string mc2 = "";
+                    string combind = (parkingGarage[R] + "|" + Input);
+
+                    for (int length = 0; length < Input.Length; length++)
+                    {
+                        if (char.IsWhiteSpace(combind[length]))
+                        {
+                            mc2 = combind.Replace(" ", "#");
+                        }
+                    }
+                    parkingGarage[R] = mc2;
+                    for (int length = 0; length < combind.Length; length++)
+                    {
+                        if (char.IsSymbol(mc2[length]))
+                        {
+                            pos = length;
+                        }
+                    }
+                    Console.WriteLine("Your spot is {0} {1}", R + 1, mc2);
+                    if (char.IsSymbol(parkingGarage[R], pos))
+                    {
+                        Console.WriteLine("testing");
+                    }
+                    //else { break; }
                 }
-                break;
             }
         }
         static void ParkingRemoving(string Input)
@@ -70,4 +88,3 @@ namespace Lab_2_Prague_Parking
         }
     }
 }
-
