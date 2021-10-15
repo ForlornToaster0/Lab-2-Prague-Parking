58f05b8866537b2c6154d62c146c479445f3402b
ForlornToaster0 <91324317+ForlornToaster0@users.noreply.github.com>
2021-10-15 11:29:47 +0200

    Update Program.cs
    finished and done

--- Lab 2 Prague Parking/Program.cs
+++ Lab 2 Prague Parking/Program.cs
@@ -13,48 +13,75 @@ namespace Lab_2_Prague_Parking
             AddingForTesting("MC#HEJ123|MC#AHA123", 78);
             AddingForTesting("CAR#AA1234BB", 33);
             AddingForTesting("MC#YRA850", 74);
+            AddingForTesting("CAR#BÜMA31", 10);
 
 
             for (int i = 0; i < 1;)
             {
                 {
-                    Console.WriteLine("Put in a command\n(N)ew vehicle\n(F)ind vehicle\n(R)emove vehicle\n(A)ll spots\n(M)ove vehicle\n(Q)uite");
+                    ParkingAll();
+                    Console.WriteLine("Put in a command\n(N)ew vehicle\n(F)ind vehicle\n(R)emove vehicle\n(M)ove vehicle\n(Q)uite");
                     string subInout1 = Console.ReadLine();
                     string mainInput1 = subInout1.ToUpper();
 
                     if (mainInput1 == "N")
                     {
+                        Console.Clear();
                         Console.WriteLine("What kind of vehicle do you have and what is it's registration number?");
                         string subInput2 = Console.ReadLine();
-                        string mainInput2 = subInput2.ToUpper();
-                        ParkingSorting(mainInput2);
+                        if (subInput2.Length < 14)
+                        {
+                            string mainInput2 = subInput2.ToUpper();
+                            ParkingSorting(mainInput2);
+                        }
+                        else
+                        { Console.WriteLine("string to long"); }
                     }
                     else if (mainInput1 == "F")
                     {
-                        Console.WriteLine("What kind of vehicle do you wnat to find?");
+                        Console.Clear();
+                       
+                        Console.WriteLine("What kind of vehicle do you want to find and what is its registration number ?");
                         string subInput3 = Console.ReadLine();
-                        string mainInput3 = subInput3.ToUpper();
-                        ParkingFinding(mainInput3);
+                        if (subInput3.Length < 14)
+                        {
+                            string mainInput3 = subInput3.ToUpper();
+                            ParkingFinding(mainInput3);
+                        }
+                        else
+                        { Console.WriteLine("string to long"); }
                     }
+
                     else if (mainInput1 == "R")
                     {
+                        Console.Clear();
+                      
                         Console.WriteLine("What is the registration number for the vehicle you want to remove?");
                         string subInput4 = Console.ReadLine();
-                        string mainInput4 = subInput4.ToUpper();
-                        ParkingRemove(mainInput4);
+                        if (subInput4.Length < 14)
+                        {
+                            string mainInput4 = subInput4.ToUpper();
+                            ParkingRemove(mainInput4);
+                        }
+                        else
+                        { Console.WriteLine("string to long"); }
                     }
-                    else if (mainInput1 == "A")
-                    {
-                        ParkingAll();
-                    }
+                   
                     else if (mainInput1 == "M")
                     {
+                        Console.Clear();
+                      
                         Console.WriteLine("What is the registration number for the car you want to move?");
                         string subInput5 = Console.ReadLine();
-                        Console.WriteLine("Which parking space do you want to move the vehicle to?");
-                        int newSpot = int.Parse(Console.ReadLine());
-                        string mainInput5 = subInput5.ToUpper();
-                        ParkingMove(mainInput5, newSpot);
+                        if (subInput5.Length < 14)
+                        {
+                            Console.WriteLine("Which parking space do you want to move the vehicle to?");
+                            int newSpot = int.Parse(Console.ReadLine());
+                            string mainInput5 = subInput5.ToUpper();
+                            ParkingMove(mainInput5, newSpot);
+                        }
+                        else
+                        { Console.WriteLine("string to long"); }
                     }
                     else if (mainInput1 == "Q")
                     {
@@ -147,14 +174,21 @@ namespace Lab_2_Prague_Parking
                 if (parkingGarage[search] == stringCombined)  // compare the input(reg-number) and the vehicle that is found. 
                 {
                     parkingGarage[search] = "";               // If they match the vehicle is removed. And the space is empty once more.
+                    Console.WriteLine("The car has been successfully removed!");
                 }
                 else if (parkingGarage[search].Contains("|"))
                 {
                     string[] subs = parkingGarage[search].Split("|", 2);  //This does the same but it's for MC.
                     if (subs[0] == stringCombined)
+                    {
                         parkingGarage[search] = subs[1];
+                        Console.WriteLine("The MC has been successfully removed!");
+                    }
                     else if (subs[1] == stringCombined)
+                    {
                         parkingGarage[search] = subs[0];
+                        Console.WriteLine("The MC has been successfully removed!");
+                    }
                 }
             }
         }
@@ -162,12 +196,13 @@ namespace Lab_2_Prague_Parking
         {
             for (int i2 = 75; i2 < 100; i2++) // This diveds the list into four parts 1-24, 25-49, 50-74, 74-100.
             {
-                Console.WriteLine($"spot[ {i2 - 74} {parkingGarage[i2 - 75]}]:  spot[ {i2 - 49} {parkingGarage[i2 - 50]}]" +
-                    $" : spot [{i2 - 24} {parkingGarage[i2 - 25]}] : spot[ {i2 + 1 } {parkingGarage[i2]}]");
+                Console.WriteLine($"spot[{i2 - 74}]: {parkingGarage[i2 - 75]}  spot[{i2 - 49}]: {parkingGarage[i2 - 50]}" +
+                    $"  spot [{i2 - 24}]: {parkingGarage[i2 - 25]}  spot[{i2 + 1 }]: {parkingGarage[i2]}");
             }
         }
         static void ParkingMove(string Input, int newPos) // Manually moves vehicles.
         {
+            newPos = newPos - 1;
             string newRegPos;
             string stringCombined = "";
             for (int length = 0; length < Input.Length; length++)
@@ -180,7 +215,7 @@ namespace Lab_2_Prague_Parking
 
             for (int search = 0; search < parkingGarage.Length; search++)  // loops through the parking garage looking for a vehicle.
             {
-                if (parkingGarage[search] == stringCombined)  // Compares the car it found and the wanted one. 
+                if (parkingGarage[search] == stringCombined && parkingGarage[newPos].Length == 0)  // Compares the car it found and the wanted one. 
                 {
                     newRegPos = parkingGarage[search];
                     parkingGarage[search] = "";          // This removes the car from the old space. It turns the space into an empty one.
@@ -193,7 +228,7 @@ namespace Lab_2_Prague_Parking
                     if (subs[0] == stringCombined)
                     {
                         newRegPos = subs[0];  //sub[0] = first part or first MC.
-                        if (parkingGarage[newPos].IndexOf("MC", 0, 2) > -1 && !parkingGarage[newPos].Contains("|")) //If the new positions isn't empty and it doesn't contain to MC 
+                        if (parkingGarage[newPos].Contains("M") && !parkingGarage[newPos].Contains("|")) //If the new positions isn't empty and it doesn't contain to MC 
                         {                                                                               //it enters the if statment.
 
                             parkingGarage[newPos] = parkingGarage[newPos] + "|" + stringCombined;   // Moves mc to a new position, and seperates the two mc's with a '|'.
@@ -209,7 +244,7 @@ namespace Lab_2_Prague_Parking
                     else if (subs[1] == stringCombined)     //this does the same as the previous if statment but it's for the second part or the second MC.
                     {
                         newRegPos = subs[1];
-                        if (parkingGarage[newPos].IndexOf("MC", 0, 2) > -1 && !parkingGarage[newPos].Contains("|"))
+                        if (parkingGarage[newPos].Contains("M") && !parkingGarage[newPos].Contains("|"))
                         {
 
                             parkingGarage[newPos] = parkingGarage[newPos] + "|" + newRegPos;
@@ -223,7 +258,7 @@ namespace Lab_2_Prague_Parking
 
                         }
                     }
-                    else { Console.WriteLine("this spot is taken"); }
+
 
                 }
             }
